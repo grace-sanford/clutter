6f0e13be2c2060efaab06b8b18ee977ee24d5908
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-12-07 16:09:26 -0500

    ciphers str

--- server/db/database.ts
+++ server/db/database.ts
@@ -1,9 +1,9 @@
 // The sole purpose of this module is to establish a connection to your
 // Postgres database by creating a Sequelize instance (called `db`).
 // You shouldn't need to make any modifications here.
-import { Sequelize } from 'sequelize';
-import pkg from '../../package.json';
-require('pg')
+import { Sequelize } from "sequelize";
+import pkg from "../../package.json";
+require("pg");
 
 const databaseUrl = process.env.DATABASE_URL;
 
@@ -12,12 +12,11 @@ const databaseUrl = process.env.DATABASE_URL;
 //   logging: false,
 // });
 if (!databaseUrl) {
-  throw new Error('DATABASE_URL environment variable is not set.');
+  throw new Error("DATABASE_URL environment variable is not set.");
 }
 
-
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-console.log("databaseUrl>>>>>>>>>>>>>>", databaseUrl)
+console.log("databaseUrl>>>>>>>>>>>>>>", databaseUrl);
 const db = new Sequelize(databaseUrl, {
   logging: (query, timing) => {
     console.log(query);
@@ -33,25 +32,17 @@ const db = new Sequelize(databaseUrl, {
     ssl: {
       require: true,
       rejectUnauthorized: false,
-      ciphers: [
-        "TLS_AES_128_GCM_SHA256",
-        "TLS_AES_256_GCM_SHA384",
-        "TLS_CHACHA20_POLY1305_SHA256",
-        "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-        "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
-        "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-      ],
+      ciphers: "TLS_AES_128_GCM_SHA256",
     },
   },
 });
 
-db
-  .authenticate()
+db.authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
   });
 
 // const db = new Sequelize('clutter', 'clutter_user', 'WHloSu3Qw23BqgV5gVl11IvyMv0oMvPZ', {
