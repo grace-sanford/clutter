3876ac0048cd9a7da33252f356eef91ac216988d
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-12-06 21:21:30 -0500

    restore server/index

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,25 @@
 const port = process.env.PORT;
 import app from "../src/pages/api"
-const db = require("./db/database").default;
+// const db = require("./db/database").default;
 
-const init = async () => {
-    try {
-      if(process.env.SEED === 'true'){
-        await seed();
-      }
-      else {
-        await db.sync()
-        console.log("Sucess syncing!")
-      }
-      // Start listening (and create a 'server' object representing our server)
-      app.listen(port, () => console.log(`Mixing it up on port ${port}`))
-    } catch (ex) {
-      console.log(ex)
-    }
-  }
+// const init = async () => {
+//     try {
+//       if(process.env.SEED === 'true'){
+//         await seed();
+//       }
+//       else {
+//         await db.sync()
+//         console.log("Sucess syncing!")
+//       }
+//       // Start listening (and create a 'server' object representing our server)
+//       app.listen(port, () => console.log(`Mixing it up on port ${port}`))
+//     } catch (ex) {
+//       console.log(ex)
+//     }
+//   }
   
-  init()
+//   init()
 
-// app.listen(port, () => {
-//  console.log("Server started on port 8000");
-// })
+app.listen(port, () => {
+ console.log("Server started on port 8000");
+})
