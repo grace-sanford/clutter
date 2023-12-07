7579d54edb8ffdc10e363d27d9cfa89643efce41
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-12-06 20:29:31 -0500

    server/index

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,23 @@
 const port = process.env.PORT;
 import app from "../src/pages/api"
 
-app.listen(port, () => {
- console.log("Server started on port 8000");
-})
+const init = async () => {
+    try {
+      if(process.env.SEED === 'true'){
+        await seed();
+      }
+      else {
+        await db.sync()
+      }
+      // Start listening (and create a 'server' object representing our server)
+      app.listen(port, () => console.log(`Mixing it up on port ${port}`))
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+  
+  init()
+
+// app.listen(port, () => {
+//  console.log("Server started on port 8000");
+// })
