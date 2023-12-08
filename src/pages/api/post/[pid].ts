459911c651447ba213e122fab1491bcc81b660ac
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-12-07 19:41:49 -0500

    fix: test dynamic route

--- src/pages/api/post/[pid].ts
+++ src/pages/api/post/[pid].ts
@@ -2,5 +2,5 @@ import type { NextApiRequest, NextApiResponse } from 'next'
  
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { pid } = req.query
-  res.end(`Post: ${pid}`)
+  res.json(`Post: ${pid}`)
 }
