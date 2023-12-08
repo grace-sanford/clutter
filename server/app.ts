fc06068db6c149c1c167ee979378421be88242ad
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-12-08 12:26:15 -0500

    fix(expressApiExample): cleanup

---
+++ server/app.ts
@@ -0,0 +1,41 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import apiRouter from '../server/expressApiExample'; 
+
+const app: Application = express();
+
+// Option to set custom cors options
+// const corsOptions = {
+//   origin: '', // Only allow requests from this origin
+//   methods: '', // Only allow these methods
+//   optionsSuccessStatus: 204, // No content for preflight OPTIONS request
+// };
+// app.use(cors(corsOptions));
+
+app.use(cors());
+
+app.use(morgan('dev'));
+
+// Body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Log incoming request bodies (helpful for debugging)
+app.use((req, res, next) => {
+    let requestBody = '';
+  
+    req.on('data', (chunk) => {
+      requestBody += chunk.toString();
+    });
+  
+    req.on('end', () => {
+      console.log('Request Body:', requestBody);
+      next();
+    });
+});
+
+// Use the API router for paths starting with "/api"
+app.use('/api', apiRouter);
+
+export default app;
