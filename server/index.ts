3f8f263fc05a35d9f26e1b592022d0c96be933c8
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-12-09 10:55:38 -0500

    docs(server/index.ts): clean up

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
-// const port = process.env.PORT;
+// Uncomment this file to use Express standalone app
+
+// const port = process.env.PORT || 8000;
 // import app from "../src/pages/api"
 
 // app.listen(port, () => {
