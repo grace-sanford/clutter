110d98ab281471948d92f57d10d5b26d5f3a623e
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-12-08 19:00:56 -0500

    fix(about): padding on small screens

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,90 +1,145 @@
-"use client"
-import React from 'react';
-import Layout from '../../components/Layout'
-import Link from 'next/link'
-import Image from 'next/image';
+"use client";
+import React from "react";
+import Layout from "../../components/Layout";
+import Link from "next/link";
+import Image from "next/image";
 
 const About = () => {
   return (
-    <Layout pageTitle={'Clutter | About'}>
-    <div className="flex flex-col h-screen">
-
-      {/* Content */}
-      <div className="flex-grow flex flex-col items-center justify-center bg-blue-600 sm:bg-white md:bg-gray-700 lg:bg-green-600 xl:bg-red-300">
-        <p className="mb-1 font-bold text-2xl">
-          How do you play?
-        </p>
-        <p>No <em>rhythms with</em>..., <em>sounds like</em>..., or <em>starts with</em>...</p>
-        <p>No sounding out or spelling out names</p>
-        <p className="mb-6">Fictional, real, famous, regular, extradordinary, funny, serious, historical and contemporary names welcome</p>
-        <p className="mb-1 font-bold text-2xl">
-          Who invented CLUTTER?
-        </p>
-        <p className="mb-6">Depending on who you ask, my Mom&apos;s friend, Denise, my Mom&apos;s friend, Lindsey, my Mom, the family in <a className="underline" href="https://en.wikipedia.org/wiki/Dan_in_Real_Life" target="_blank"><em>Dan In Real Life</em></a>, or this game is  
-        actually just the first round of that other party game, <a className="underline" href="https://thenerdy.com/how-to-play-salad-bowl/" target="_blank">Salad Bowl</a></p>
-        <p className="mb-1 font-bold text-2xl">
-          What&apos;s the deal with this website?
-        </p>
-        <p className="mb-1">Developed with <a href='https://bun.sh/' target="_blank" 
-        className="underline" >Bun</a> and <a href='https://chat.openai.com/' className="underline" target="_blank">ChatGPT</a>, 
-        bootstraped with <a href="https://nextjs.org/" 
-        target="_blank" className="underline">NextJS</a> frontend and <a href='https://www.postgresql.org/' 
-        target="_blank" className="underline">PostreSQL</a> backend,
-        this project is the work of me, <a href='https://github.com/grace-sanford' target="_blank" className="underline font-mono font-bold">
-        @gracesanford</a> </p> 
-        <div className='flex flex-row space-x-4 items-center'>
-  <a href="https://bun.sh/" target="_blank" rel="noreferrer">
-    <Image
-      src="/bun-logo.svg" 
-      alt="bun" 
-      width={50} 
-      height={50}
-      className="logo"
-    />
-  </a>
-  <a href="https://chat.openai.com/" target="_blank" rel="noreferrer">
-    <Image
-      src="/chatgpt-logo.svg" 
-      alt="postresql" 
-      width={50} 
-      height={50}
-      className="logo"
-    />
-  </a>
-  <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-    <Image
-      src="/next-logo.svg" 
-      alt="postresql" 
-      width={50} 
-      height={50}
-      className="logo"
-    />
-  </a>
-  <a href="https://www.postgresql.org" target="_blank" rel="noreferrer">
-    <Image 
-      src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" 
-      alt="postgresql" 
-      width={50} 
-      height={50}
-      className="logo"
-    /> 
-  </a>
-</div>
-        <p>Landing page line art is courtesy of Kristina Margaryan from{' '}
-          <a
-            href="https://thenounproject.com/browse/icons/term/line-art/"
-            className="underline"
-            target="_blank"
-            title="line art Icons"
+    <Layout pageTitle={"Clutter | About"}>
+      <div className="flex flex-col h-screen">
+        {/* Content */}
+        <div className="flex-grow flex flex-col items-center justify-center bg-blue-600 sm:bg-white md:bg-gray-700 lg:bg-green-600 xl:bg-red-300 mx-auto p-4">
+          <p className="mb-1 font-bold text-2xl">How do you play?</p>
+          <p>
+            No <em>rhythms with</em>..., <em>sounds like</em>..., or{" "}
+            <em>starts with</em>...
+          </p>
+          <p>No sounding out or spelling out names</p>
+          <p className="mb-6">
+            Fictional, real, famous, regular, extradordinary, funny, serious,
+            historical and contemporary names welcome
+          </p>
+          <p className="mb-1 font-bold text-2xl">Who invented CLUTTER?</p>
+          <p className="mb-6">
+            Depending on who you ask, my Mom&apos;s friend, Denise, my
+            Mom&apos;s friend, Lindsey, my Mom, the family in{" "}
+            <a
+              className="underline"
+              href="https://en.wikipedia.org/wiki/Dan_in_Real_Life"
+              target="_blank"
+            >
+              <em>Dan In Real Life</em>
+            </a>
+            , or this game is actually just the first round of that other party
+            game,{" "}
+            <a
+              className="underline"
+              href="https://thenerdy.com/how-to-play-salad-bowl/"
+              target="_blank"
+            >
+              Salad Bowl
+            </a>
+          </p>
+          <p className="mb-1 font-bold text-2xl">
+            What&apos;s the deal with this website?
+          </p>
+          <p className="mb-1">
+            Developed with{" "}
+            <a href="https://bun.sh/" target="_blank" className="underline">
+              Bun
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://chat.openai.com/"
+              className="underline"
+              target="_blank"
+            >
+              ChatGPT
+            </a>
+            , bootstraped with{" "}
+            <a href="https://nextjs.org/" target="_blank" className="underline">
+              NextJS
+            </a>{" "}
+            frontend and{" "}
+            <a
+              href="https://www.postgresql.org/"
+              target="_blank"
+              className="underline"
+            >
+              PostreSQL
+            </a>{" "}
+            backend, this project is the work of me,{" "}
+            <a
+              href="https://github.com/grace-sanford"
+              target="_blank"
+              className="underline font-mono font-bold"
+            >
+              @gracesanford
+            </a>{" "}
+          </p>
+          <div className="flex flex-row space-x-4 items-center">
+            <a href="https://bun.sh/" target="_blank" rel="noreferrer">
+              <Image
+                src="/bun-logo.svg"
+                alt="bun"
+                width={50}
+                height={50}
+                className="logo"
+              />
+            </a>
+            <a href="https://chat.openai.com/" target="_blank" rel="noreferrer">
+              <Image
+                src="/chatgpt-logo.svg"
+                alt="postresql"
+                width={50}
+                height={50}
+                className="logo"
+              />
+            </a>
+            <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+              <Image
+                src="/next-logo.svg"
+                alt="postresql"
+                width={50}
+                height={50}
+                className="logo"
+              />
+            </a>
+            <a
+              href="https://www.postgresql.org"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+                alt="postgresql"
+                width={50}
+                height={50}
+                className="logo"
+              />
+            </a>
+          </div>
+          <p>
+            Landing page line art is courtesy of Kristina Margaryan from{" "}
+            <a
+              href="https://thenounproject.com/browse/icons/term/line-art/"
+              className="underline"
+              target="_blank"
+              title="line art Icons"
+            >
+              Noun Project
+            </a>{" "}
+            (CC BY 3.0)
+          </p>
+          {/* "Play Clutter" button */}
+          <Link
+            href="/"
+            className="bg-green-500 text-white py-3 px-6 rounded-full font-bold text-xl mt-8"
           >
-            Noun Project
-          </a>{' '}
-          (CC BY 3.0)</p>
-        {/* "Play Clutter" button */}
-        <Link href="/" className="bg-green-500 text-white py-3 px-6 rounded-full font-bold text-xl mt-8">
-          Play CLUTTER
+            Play CLUTTER
           </Link>
-      </div>
+        </div>
       </div>
     </Layout>
   );
