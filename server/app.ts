3ef80770d87fec4819d5f1a16fdf166593ded4f8
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-12-09 10:47:42 -0500

    docs(app.ts): clean up

--- server/app.ts
+++ server/app.ts
@@ -1,21 +1,13 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import apiRouter from '../server/expressApiExample'; 
+import express, { Application } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import apiRouter from "../server/expressApiExample";
 
 const app: Application = express();
 
-// Option to set custom cors options
-// const corsOptions = {
-//   origin: '', // Only allow requests from this origin
-//   methods: '', // Only allow these methods
-//   optionsSuccessStatus: 204, // No content for preflight OPTIONS request
-// };
-// app.use(cors(corsOptions));
-
 app.use(cors());
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Body parsing middleware
 app.use(express.json());
@@ -23,19 +15,19 @@ app.use(express.urlencoded({ extended: true }));
 
 // Log incoming request bodies (helpful for debugging)
 app.use((req, res, next) => {
-    let requestBody = '';
-  
-    req.on('data', (chunk) => {
-      requestBody += chunk.toString();
-    });
-  
-    req.on('end', () => {
-      console.log('Request Body:', requestBody);
-      next();
-    });
+  let requestBody = "";
+
+  req.on("data", (chunk) => {
+    requestBody += chunk.toString();
+  });
+
+  req.on("end", () => {
+    console.log("Request Body:", requestBody);
+    next();
+  });
 });
 
 // Use the API router for paths starting with "/api"
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 export default app;
